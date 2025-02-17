dc738d875662dabda01a3845db41bde8894834ec
ArulPrakash197 <arulprakash197@gmail.com>
2025-02-18 00:08:19 +0530

    database itergration part done

--- pythonProject/HTML/script.js
+++ pythonProject/HTML/script.js
@@ -1,6 +1,35 @@
-btn = document.querySelector(".share");
-forms = document.querySelector(".fact-form");
-vote_button = document.querySelector(".vote-button");
+async function loadparams() {
+  res = await fetch("https://bhjjuemrkslhazblfxpe.supabase.co/rest/v1/facts", {
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoamp1ZW1ya3NsaGF6YmxmeHBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MDA2NDYsImV4cCI6MjA1NTI3NjY0Nn0.J4TEhdwhN-oxtQQrc0lJZ6d7DG8fC2l0gwXw21AKVy0",
+      authorization:
+        " Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoamp1ZW1ya3NsaGF6YmxmeHBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MDA2NDYsImV4cCI6MjA1NTI3NjY0Nn0.J4TEhdwhN-oxtQQrc0lJZ6d7DG8fC2l0gwXw21AKVy0",
+    },
+  });
+  console.log(res);
+  data = await res.json();
+  createFactlist(data);
+}
+loadparams();
+let btn = document.querySelector(".share");
+let forms = document.querySelector(".fact-form");
+let formlist = document.querySelector(".fact-list");
+formlist.innerHTML = "";
+function createFactlist(dataArray) {
+  //adding facts
+  const htmlattr = dataArray.map(
+    (fact) => `<li class="fact">
+                  <p>${fact.text}</p>
+                  <a class="source" href=${fact.source} target="_blank">More Info</a>
+                  <span class="tag">${fact.category}</span></li>`
+  );
+  const html = htmlattr.join("");
+  formlist.insertAdjacentHTML("afterbegin", html);
+}
+//addingcategory
+
+//sharebutton
 btn.addEventListener("click", function () {
   if (forms.classList.contains("fact-form")) {
     forms.classList.remove("fact-form");
@@ -10,6 +39,5 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share";
   }
 });
-formlist = document.querySelector(".fact-list");
+
 console.dir(formlist);
-formlist.innerHTML = "";
